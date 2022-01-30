d9ec0df84c4dc5b7b7581ca43eb4d96c708b85b7
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-01-30 00:28:57 +0000

    Cleaned formatting

--- arithmetic.py
+++ arithmetic.py
@@ -5,11 +5,9 @@ def add(num1, num2):
     """Return the sum of num1 and num2."""
     return num1 + num2
 
-
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
     return num1 - num2
-
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
@@ -19,7 +17,6 @@ def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
     return num1 / num2
 
-
 def square(num1):
     """Return the square of num1."""
     return num1 * num1
@@ -27,7 +24,6 @@ def square(num1):
 def cube(num1):
     """Return the cube of num1."""
     return num1**3
-
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
